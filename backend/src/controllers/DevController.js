43cc4d67bdedefef788ca5b76580f1798a8d0524
Vitor Bonet <vitorbonet@hotmail.com>
2020-03-09 20:13:16 -0300

    initial commit

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,55 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
+const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections } = require('../websocket');
+// index, show, store, update, destroy
+
+module.exports = {
+
+    async index(req, res) {
+        const devs = await Dev.find();
+
+        return res.json(devs);
+    },
+
+    async store(req, res) {
+        const { github_username, techs, latitude, longitude } = req.body;
+    
+        let dev = await Dev.findOne({ github_username });
+
+        if (!dev) {
+            const apiResponse = await axios.get(`https://api.github.com/users/${github_username} `);
+    
+            const { name = login, avatar_url, bio } = apiResponse.data;
+        
+            const techArray = parseStringAsArray(techs);
+        
+            const techsArray = parseStringAsArray(techs);
+
+            const location = {
+                type: 'Point',
+                coordinates: [longitude, latitude],
+            }
+        
+            dev = await Dev.create({
+                github_username,
+                name,
+                avatar_url,
+                bio,
+                techs: techArray,
+                location,
+            });
+
+            // Filtrar conexões que estão no raio e tenham 1 das techs
+            const sendSocketMessageTo = findConnections(
+                { latitude, longitude },
+                techsArray,
+            );
+
+            console.log(sendSocketMessageTo);
+        }
+    
+        return res.json( dev );
+    }
+};
---
+++ backend/src/controllers/SearchController.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+// index, show, store, update, destroy
+
+module.exports = {
+
+    async index(req, res) {
+        const { latitude, longitude, techs } = req.query;
+
+        const techsArray = parseStringAsArray(techs);
+
+        const devs = await Dev.find({
+            techs: {
+                $in: techsArray,
+            },
+            location: {
+                $near: {
+                    $geometry: {
+                        type: 'Point',
+                        coordinates: [longitude, latitude],
+                    },
+                    $maxDistance: 100000,
+                },
+            },
+        });
+
+        return res.json({ devs });
+    },
+}
